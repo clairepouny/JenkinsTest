50caaa0424b6e15cb7c54c493b35f7f69c7706b3
clairepouny <47818914+clairepouny@users.noreply.github.com>
2020-11-09 10:17:13 +0100

    Add files via upload

---
+++ SimpleTest.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.assertTrue;
+import org.junit.Test;
+
+public class SimpleTest {
+	
+	@Test
+	public void test1() {
+		int a=2;
+		int b=2;
+		assertTrue(a+b==4);
+	}
+	
+	@Test
+	public void test2() {
+		int a=2;
+		int b=2;
+		assertTrue(a+b<4);
+	}
+
+}
